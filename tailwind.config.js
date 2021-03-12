9468a977755c098689b70507b1e3e577c2907cf4
Ohad <ohadosnatcv@gmail.com>
2021-03-12 15:39:58 +0200

    At tailwind.config.js - added a global transition class, zIndex, custom grid setup and finished the car music player page - car-music.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      zIndex: {
+        '-1': '-1',
+      },
       fontFamily: {
         'roboto': ['Roboto, sans-serif']
       },
+      gridTemplateColumns: {
+        // Main Grid config - min-content
+        'main': "min-content 1fr min-content",
+      },
+      gridTemplateRows: {
+        // Main Grid config - min-content
+        'main': "min-content auto min-content",
+      },
+      backgroundImage: theme => ({
+        'awaken-mob': "url('../img/img_awaken-my-love-mob.jpg')",
+        'awaken-desk': "url('../img/img_awaken-my-love-desktop.jpg')",
+      })
     },
   },
   variants: {
